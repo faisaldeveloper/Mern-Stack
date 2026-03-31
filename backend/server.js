e76a065f0719cb44615e2e15b16e918647e41a7e
Faisal Shahzad <faisal.shahzad786@gmail.com>
2026-03-31 10:22:44 +0500

    Minor tweak in server.js file

--- backend/server.js
+++ backend/server.js
@@ -22,20 +22,17 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 
-// Test route
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
-
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, 'frontend/dist')));
 
 	app.get(/^(?!\/api).+/, (req, res) => {
 		res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
 	});
+} else {
+	app.get('/', (req, res) => {
+		res.send('API is running...');
+	});
 }
-
-
 
 app.listen(PORT, async () => {
 	await connectDB();
